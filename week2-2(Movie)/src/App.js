740e39d3765fd8f69a870a54a8e7c8de375f1d47
Daeuni <ekdmsjj@gmail.com>
2024-10-04 02:43:33 +0900

    week2_movie

--- week2-2(Movie)/src/App.js
+++ week2-2(Movie)/src/App.js
@@ -1,18 +1,12 @@
-import './App.css';
-import { MOVIES } from './mocks/movie';
 import Poster from './Components/Poster';
+import React, { useState } from "react";
+
 
 function App() {
   return (
-    <div className="movies">
-    {MOVIES.results.map(a => (
-      <Poster
-        key={a.id}
-        title={a.title}
-        poster_path={a.poster_path}
-      />
-    ))}
-  </div>
+    <>
+      <Poster/>
+    </>
   )
 };
 
