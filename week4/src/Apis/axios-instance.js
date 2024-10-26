869f303302af7c97179ca84fd09e06f858cfb3b3
Daeuni <ekdmsjj@gmail.com>
2024-10-26 23:24:32 +0900

    오타수정

--- week4/src/Apis/axios-instance.js
+++ week4/src/Apis/axios-instance.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     headers:{
-        Authorization: `Bearer ${import.meta.env.VITE_TMDM_TOKEN}`
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`
     },
-    baseURL:import.meta.env.VITE_TMDM_URL
+    baseURL:import.meta.env.VITE_TMDB_URL
 })
 export {axiosInstance}
