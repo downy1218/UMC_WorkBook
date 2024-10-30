e2e7cfcb0703f599c5a49f56bdf0bb3f89401bd4
Daeuni <ekdmsjj@gmail.com>
2024-10-30 16:42:17 +0900

    yup

--- week5/src/Pages/Register.jsx
+++ week5/src/Pages/Register.jsx
@@ -1,11 +1,30 @@
 import {useForm} from 'react-hook-form';
-
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 function RegisterPage(){
+    const schema = yup.object().shape({
+        이메일 :yup.string().email().required('이메일을 입력해주세요'),
+        비밀번호: yup.string().min(8, '최소 8글자 이상입력하세요').max(14,'최대 14글자까지 가능합니다').required('비밀번호를 입력헤주세요')
+    })
+    const {register, handleSubmit, formState:{errors}} = useForm({
+        resolver: yupResolver(schema)
+    });
+    const onSubmit = (data)=>{
+        console.log('폼제출')
+        console.log(data)
+    };
+
+
+
     return(
-        <>
-            <p style={{color:'white',fontSize:'40px', marginLeft:'150px'}}>회원가입 페이지 입니다</p>
-        </>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type={'email'}{...register('이메일')}/>
+            <p style={{color: 'red'}}>{errors.이메일?.message}</p>
+            <input type={'password'}{...register('비밀번호')}/>
+            <p style={{color: 'red'}}>{errors.비밀번호?.message}</p>
+            <input type={'submit'}/>
+        </form>
     )
 }
 export default RegisterPage;
