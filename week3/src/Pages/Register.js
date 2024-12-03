ffaf15db2a7f639b20a487b44fbecb126399ced9
Daeuni <ekdmsjj@gmail.com>
2024-12-04 03:18:40 +0900

    .

--- week3/src/Pages/Register.js
+++
@@ -1,8 +0,0 @@
-function RegisterPage(){
-    return(
-        <>
-            <p style={{color:'white',fontSize:'40px', marginLeft:'150px'}}>회원가입 페이지 입니다</p>
-        </>
-    )
-}
-export default RegisterPage;
--- week4/src/Apis/axios-instance.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-    headers:{
-        Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`
-    },
-    baseURL:import.meta.env.VITE_TMDB_URL
-})
-export {axiosInstance}
